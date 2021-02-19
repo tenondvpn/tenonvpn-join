4558312564d4f9819d0a56381edf290b2cc8ca10
root <root@192.168.189.207>
2021-02-20 00:05:36 +0800

    support nat and vps

--- init_env.sh
+++ init_env.sh
@@ -27,6 +27,29 @@ cd /etc/rc.d/init.d/ && chkconfig --add fast_net.sh && chkconfig fast_net.sh on
 cd $prevwd
 install() {
     if [[ "${release}" == "centos" ]]; then
+        if [ $# != 2 ] ; then
+            ins_path=`pwd`
+            min_port=0
+            second_port=0
+            third_port=0
+            max_port=0
+            sed -i -e 's/replace_min_port/'$min_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_second_port/'$second_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_third_port/'$third_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_max_port/'$max_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_vpn_server_port/1/g' $ins_path/node/conf/lego.conf            
+        else
+            ins_path=`pwd`
+            min_port=$1
+            second_port=`expr $1 + 1`
+            third_port=`expr $1 + 2`
+            max_port=$2
+            sed -i -e 's/replace_min_port/'$min_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_second_port/'$second_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_third_port/'$third_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_max_port/'$max_port'/g' $ins_path/node/conf/lego.conf
+            sed -i -e 's/replace_vpn_server_port/0/g' $ins_path/node/conf/lego.conf
+        fi
 	sudo sh centos_env.sh
     else
         echo "just support centos7."
